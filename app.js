0c4bed64eb5d3fea50325a54268f1d2e532d4081
bngcminh <buingocminh2k4@gmail.com>
2026-01-29 22:02:48 +0700

    csm

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-import Fastify from 'fastify';
-const fastify = Fastify({logger: true});
+const fastify = require('fastify')({ logger: true });
 
 fastify.get('/', (req, rep) => {
-    rep.send('Test server');
+    rep.send('hello world');
 })
 
-fastify.listen({host: '127.0.0.1',port: 3000}, (err, address) => {
+fastify.listen({ port: 3000 }, (err) => {
     if(err){
         fastify.log.error(err);
-        process.exit(1);
-    }else{
-        console.log(address);
+        process.exit(1)
     }
 })
