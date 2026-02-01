a0a415de054f4f9879645400806bada4cb0a0ed4
bngcminh <buingocminh2k4@gmail.com>
2026-02-01 21:22:57 +0700

    test server

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+require('dotenv').config()
+
 const fastify = require('fastify')({ logger: true });
+const fastifyStatic = require('@fastify/static')
+const fastifyViews = require('@fastify/view');
+const path = require('node:path');
+const connectDB = require('@fastify/mongodb');
+
+// Fix Error: querySrv ECONNREFUSED MongoDB
+const dns = require('node:dns/promises');
+dns.setServers(['1.1.1.1']);
+
+// Connect MongoDB
+fastify.register(connectDB, {
+    forceClose: false,
+    url: process.env.DATABASE
+})
+
+// View file
+fastify.register(fastifyViews, {
+    engine: {
+        pug: require('pug'),
+    },
+    root: path.join(__dirname, ('views'))
+})
+
+// Static file
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/'
+})
 
 fastify.get('/', (req, rep) => {
-    rep.send('hello world');
+    // rep.send('hello world');
+    rep.view("home.pug");
 })
 
 fastify.listen({ port: 3000 }, (err) => {
     if(err){
         fastify.log.error(err);
         process.exit(1);
+    }else{
+
     }
 })
---
+++ config/db.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose');
+
+// const connect = async function () {
+//     try {
+//         await mongoose.connect(process.env.DATABASE);
+//         console.log('Connect MongoDB Success!');
+//     }catch (error) {
+//         console.log('Connect MongoDB Failure!');
+//         console.log(error);
+//         process.exit(1);
+//     }
+// }
+
+// module.exports = connect;
