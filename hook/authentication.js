1f9c96149bdc305ce14e9c2cd2b2b4fa8f107738
bngcminh <buingocminh2k4@gmail.com>
2026-04-11 13:01:59 +0700

    fix hook

--- hook/authentication.js
+++ hook/authentication.js
@@ -1,11 +1,12 @@
-function authentication(req, rep){
+async function authentication(req, rep){
     try{
+        // await req.jwtVerify()
         const token = req.cookies?.token;
         if(token){
             const user = req.server.jwt.verify(token);
             req.user = user;
         }else{
-            rep.redirect('/');
+            req.user = null;
         }
         console.log("result user:", req.user);
     }catch(err){
