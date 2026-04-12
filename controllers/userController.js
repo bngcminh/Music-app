a7e3ab97c083531f85152ac241a53b6d5894d3c7
bngcminh <buingocminh2k4@gmail.com>
2026-04-13 00:47:15 +0700

    update usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -145,6 +145,56 @@ const deletePlaylist = async function(req, rep){
     }
 }
 
+const getSelectPlaylistPage = async function(req, rep) {
+    try {
+        const songId = req.params.songId;
+        const song = await Song.findById(songId).populate('artist', 'name avatar');
+
+        if (!song) {
+            return rep.code(404).send('Khong tim thay bai hat');
+        }
+
+        const playlists = await UserPlaylist.find({ user: req.user.id });
+
+        return rep.view('select_playlist.pug', {
+            song,
+            playlists,
+            user: req.user,
+            success: req.query.success === '1'
+        });
+    } catch (err) {
+        console.log(err);
+        return rep.code(500).send('Co loi khi tai trang chon playlist');
+    }
+};
+
+const addSongToPlaylist = async function(req, rep) {
+    try {
+        const songId = req.params.songId;
+        const { playlistId } = req.body;
+
+        const song = await Song.findById(songId);
+        if (!song) {
+            return rep.code(404).send('Khong tim thay bai hat');
+        }
+
+        const playlist = await UserPlaylist.findOneAndUpdate(
+            { _id: playlistId, user: req.user.id },
+            { $addToSet: { songs: songId } },
+            { new: true }
+        );
+
+        if (!playlist) {
+            return rep.code(404).send('Khong tim thay playlist');
+        }
+
+        return rep.redirect(`/song/${songId}/select-playlist?success=1`);
+    } catch (err) {
+        console.log(err);
+        return rep.code(500).send('Co loi khi them bai hat vao playlist');
+    }
+};
+
 const changePassword = async function(req, rep){
     try {
         const userId = req.user.id;
@@ -183,5 +233,7 @@ module.exports = {
     createPlaylist,
     updatePlaylist,
     deletePlaylist,
+    getSelectPlaylistPage,
+    addSongToPlaylist,
     changePassword
 };
--- routes/playlistPickerRoute.js
+++
@@ -1,9 +0,0 @@
-const authentication = require('../hook/authentication');
-const playlistPickerController = require('../controllers/playlistPickerController');
-
-function playlistPickerRoute(fastify, options) {
-    fastify.get('/song/:songId/select-playlist', { preHandler: authentication }, playlistPickerController.getSelectPlaylistPage);
-    fastify.post('/song/:songId/select-playlist', { preHandler: authentication }, playlistPickerController.addSongToPlaylist);
-}
-
-module.exports = playlistPickerRoute;
